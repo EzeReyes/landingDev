f097a6af42cf9ee0ba58429d5bff9ab89000484e
ezequiel reyes <110490540+EzeReyes@users.noreply.github.com>
2025-03-22 18:21:56 -0300

    first commit

--- config/apollo.js
+++ config/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink , InMemoryCache } from "@apollo/client";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:4000/graphql", // Reemplaza con tu endpoint de GraphQL
+    uri: "https://landinggraphql.onrender.com/graphql", // Reemplaza con tu endpoint de GraphQL
 });
 
 const authLink = setContext((_, { headers }) => {
